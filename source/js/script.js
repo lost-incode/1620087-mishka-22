afb5ea6ea8e413b75858a8c653ab6f67856ea532
lost-incode <vicktoriaap@gmail.com>
2021-05-10 17:55:39 +0300

    Add modal window with js code to index.html and catalog.html

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,7 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const modalLinks = document.querySelectorAll('.cart-link');
+const modalWindow = document.querySelector('.modal__wrapper');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -10,5 +12,26 @@ navToggle.addEventListener('click', function() {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+for (const modalLink of modalLinks) {
+  modalLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalWindow.classList.remove('modal--close');
+    modalWindow.classList.add('modal--open');
+  });
+}
+
+document.addEventListener('keyup', function (event) {
+  if (event.defaultPrevented) {
+      return;
+  }
+
+  var key = event.key || event.keyCode;
+
+  if (key === 'Escape' || key === 'Esc' || key === 27) {
+    modalWindow.classList.remove('modal--open');
+    modalWindow.classList.add('modal--close');
   }
 });
